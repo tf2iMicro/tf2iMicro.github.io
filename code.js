8a56879adc70520e400503ec41dce99afec6b080
EirikAufSchule <e-eirgee@iskule.no>
2022-11-08 18:56:19 +0100

    Endret endring on scroll på navbar

--- code.js
+++ code.js
@@ -3,14 +3,14 @@ let picOne = document.getElementById("discPicOne")
 picOne.addEventListener("click",imgBig)
 function imgBig(){picOne.classList.toggle("active")}
 /* minker høyden til navbar ved scroll ned */
-/*var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.querySelector("#navbar").style.height = "12vh"
     } 
     else {
-        document.querySelector("#navbar").style.height = "10vh"
+        document.querySelector("#navbar").style.height = "8vh"
     }
     prevScrollpos = currentScrollPos;
-}*/
+}
