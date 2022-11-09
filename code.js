d5b39041b45d121ceec8d01247b4ad2ebb3dc2ee
jasunk <jskraakenes@gmail.com>
2022-11-09 10:16:26 +0100

    hehe, easteregg
    try typing "imicro" when visiting site

--- code.js
+++ code.js
@@ -1,17 +1,37 @@
-let picOne = document.getElementById("discPicOne")
+let desiredString = "imicro"
+let currentString = ""
 
-picOne.addEventListener("click",imgBig)
-function imgBig(){picOne.classList.toggle("active")}
-/* minker høyden til navbar ved scroll ned
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector("#navbar").style.height = "12vh"
-    } 
-    else {
-        document.querySelector("#navbar").style.height = "8vh"
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 73) {
+        /* i */
+        currentString+="i"
     }
-    prevScrollpos = currentScrollPos;
-}
-*/
+    else if(event.keyCode == 77) {
+        /* m */
+        currentString+="m"
+    }
+    else if(event.keyCode == 67) {
+        /* c */
+        currentString+="c"
+    }
+    else if(event.keyCode == 82) {
+        /* r */
+        currentString+="r"
+    }
+    else if(event.keyCode == 79) {
+        /* o */
+        currentString+="o"
+    }
+    else {
+        currentString= ""
+    }
+    
+    if (currentString==desiredString){
+        document.getElementById("navbarTitle").innerHTML = "Hehe, easteregg"
+    }
+    
+    
+    
+
+});
+
