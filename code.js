8b17c1f98a3cccdbb9f2ff0b38b13d3135f28d67
EirikAufSchule <e-eirgee@iskule.no>
2022-11-09 13:04:15 +0100

    La icon og tekst for navbarknapper på linje

--- code.js
+++ code.js
@@ -21,7 +21,17 @@ document.addEventListener('keydown', function(event) {
     else if(event.keyCode == 79) {
         /* o */
         currentString+="o"
-    }
+    }    
+/* picOne.addEventListener("click",imgBig) */
+function imgBig(){picOne.classList.toggle("active")};
+});
+/* minker høyden til navbar ved scroll ned
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector("#navbar").style.height = "12vh"
+    } 
     else {
         currentString= ""
     }
@@ -29,9 +39,17 @@ document.addEventListener('keydown', function(event) {
     if (currentString==desiredString){
         document.getElementById("navbarTitle").innerHTML = "Hehe, easteregg"
     }
-    
-    
-    
-
 });
+    prevScrollpos = currentScrollPos;
+}
+*/
 
+const commentContainer = document.querySelector(".otherArea");
+const featureComment = document.querySelector(".bulletPoint");
+
+window.onscroll = function(){
+    console.log(commentContainer.scrollTop);
+    /* if(document.querySelector("body").scrollTop == featureComment.style.padding){
+
+    } */
+}
